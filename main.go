dabb6ec50ecd3f5d215332431f6baef708f0b444
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2025-07-22 18:04:51 +0530

    update: init go project

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+
+	r := gin.Default()
+
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	r.Run(":8080")
+}
