57251ba0ad5ec4ebb9ce0b2cc93466abc41014f2
Chetan Singh Sajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2025-08-25 05:06:04 +0000

    update: add routes

--- main.go
+++ main.go
@@ -2,43 +2,10 @@ package main
 
 import (
 	"tasks/db"
-	"tasks/handlers"
+	"tasks/routes"
 	"tasks/services"
 	"tasks/store/pg"
-
-	"github.com/gin-gonic/gin"
 )
-
-func setupRouter() *gin.Engine {
-
-	var r = gin.Default()
-
-	r.POST("/auth/login", handlers.Login)
-	r.POST("/api/users", handlers.CreateUser)
-
-	r.Group("/api", handlers.AuthenticateMiddleware)
-	{
-		r.POST("/tasks", handlers.CreateTask)
-		r.GET("/tasks/:id", handlers.GetTask)
-		r.PATCH("/tasks/:id", handlers.UpdateTask)
-		r.DELETE("/tasks/:id", handlers.DeleteTask)
-
-		r.GET("/users/:id", handlers.GetUser)
-		r.PATCH("/users/:id", handlers.UpdateUser)
-		r.DELETE("/users/:id", handlers.DeleteUser)
-
-		r.POST("/lists", handlers.CreateList)
-		r.GET("/lists/:id", handlers.GetList)
-		r.PATCH("/lists/:id", handlers.UpdateList)
-		r.DELETE("/lists/:id", handlers.DeleteList)
-
-		r.POST("/lists/access/:id", handlers.AddListAccess)
-		r.GET("/lists/access/:id", handlers.GetListAccess)
-		r.DELETE("/lists/access/:id", handlers.RemoveListAccess)
-	}
-
-	return r
-}
 
 func main() {
 
@@ -48,6 +15,6 @@ func main() {
 		Pool: db.Pool,
 	}
 
-	var r = setupRouter()
+	var r = routes.SetupRouter()
 	r.Run(":8080")
 }
---
+++ routes/router.go
@@ -0,0 +1,38 @@
+package routes
+
+import (
+	"tasks/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+
+	var r = gin.Default()
+
+	r.POST("/auth/login", handlers.Login)
+	r.POST("/api/users", handlers.CreateUser)
+
+	r.Group("/api", handlers.AuthenticateMiddleware)
+	{
+		r.POST("/tasks", handlers.CreateTask)
+		r.GET("/tasks/:id", handlers.GetTask)
+		r.PATCH("/tasks/:id", handlers.UpdateTask)
+		r.DELETE("/tasks/:id", handlers.DeleteTask)
+
+		r.GET("/users/:id", handlers.GetUser)
+		r.PATCH("/users/:id", handlers.UpdateUser)
+		r.DELETE("/users/:id", handlers.DeleteUser)
+
+		r.POST("/lists", handlers.CreateList)
+		r.GET("/lists/:id", handlers.GetList)
+		r.PATCH("/lists/:id", handlers.UpdateList)
+		r.DELETE("/lists/:id", handlers.DeleteList)
+
+		r.POST("/lists/access/:id", handlers.AddListAccess)
+		r.GET("/lists/access/:id", handlers.GetListAccess)
+		r.DELETE("/lists/access/:id", handlers.RemoveListAccess)
+	}
+
+	return r
+}
